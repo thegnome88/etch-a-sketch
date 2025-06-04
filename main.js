b85d4c81d2caefe947b0ec89297f4d706df2f854
thegnome88 <thegnomegaming@gmail.com>
2025-06-03 18:04:31 -0700

    Complete functionality for everything

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let canvas = document.querySelector('.canvas');
 let setSizeButton = document.querySelector('.set-size');
+let colors = document.querySelector('.colors'); 
 
-const SIZE_OF_CANVAS = canvas.style.height;
-console.log(SIZE_OF_CANVAS)
+const SIZE_OF_CANVAS = parseFloat(window.getComputedStyle(canvas).height);
+let color = 'black';
 
 function setCanvasSize() {
     let size = prompt('What size would you like the canvas to be? MAX: 50');
@@ -10,8 +11,10 @@ function setCanvasSize() {
 
     if (size === NaN || !Number.isInteger(size) || size > 50) {
         alert('Invalid canvas size!');
+        return;
     }
 
+    canvas.innerHTML = '';
     createSquares(size);
 }
 
@@ -22,9 +25,33 @@ function createSquares(size) {
         console.log(newSize);
         newDiv.style.height = `${newSize}px`;
         newDiv.style.width = `${newSize}px`;
+        newDiv.style.boxSizing = 'border-box';
+        newDiv.style.border = '1px solid rgb(233, 233, 233)';
+        newDiv.addEventListener('mouseenter', () => {
+            newDiv.style.backgroundColor = color;
+            newDiv.style.borderColor = color;
+        })
+
 
         canvas.append(newDiv);
     }
 }
 
+function setColor(e) {
+    let button = e.target;
+
+    switch (button.id) {
+        case 'black':
+            color = 'black';
+            break;
+        case 'blue':
+            color = 'blue';
+            break;
+        case 'red':
+            color = 'red';
+            break;
+    }
+}
+
 setSizeButton.addEventListener('click', setCanvasSize);
+colors.addEventListener('click', setColor);
